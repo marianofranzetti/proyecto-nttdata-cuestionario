8b05a7d1d51fe17ea0009fbff0084a2d861a07b5
Mariano Andres Franzetti <mfranzet@emeal.nttdata.com>
2021-12-13 11:21:32 -0300

    hola

---
+++ preguntados-mariano/src/test/java/com/example/preguntadosmariano/PreguntadosMarianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preguntadosmariano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntadosMarianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
